545cd0f3a1a27502cc498f44b39f6782f406c89a
Hpayeur <hailpaye017@gmail.com>
2026-02-03 13:01:02 -0700

    putting everything together

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Hello, Everyone! Welome to my fianal project!");
+console.log("I hope you enjoy it!");
+console.error("Just kidding, there is no final project! :D");
+console.log("Have a great day!");
+console.log("Goodbye! for now...");
